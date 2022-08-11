944c38a6143783707801ac6cf985b8ddd79c18af
Cristian David Ippolito <cristiandavidippolito@gmail.com>
2022-08-11 03:20:30 +0000

    feat: updating BASIC_ACL_API_BASE_URL value

--- lib/basic-acl.constants.d.ts
+++ lib/basic-acl.constants.d.ts
@@ -1,2 +1,2 @@
 export declare const BASIC_ACL_CONFIG_TOKEN: unique symbol;
-export declare const BASIC_ACL_API_BASE_URL = "https://basic-acl-api-pro.herokuapp.com/";
+export declare const BASIC_ACL_API_BASE_URL = "https://api.basic-acl.com/";
